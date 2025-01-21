b5bf229e8e7e521b1d4d753d679e06dc1b857b45
sthevanbello <sthevan.alves@gmail.com>
2025-01-21 17:38:55 -0300

    Finalizada a etapa de manipulação de strings

---
+++ 01 - Crie a sua primeira aplicacao/strings.py
@@ -0,0 +1,11 @@
+print('Sabor Express')
+print('*'*50)
+print(f'Menu')
+print('1 - Cadastrar restaurantes')
+print('2 - Listar restaurantes')
+print('3 - Ativar restaurantes')
+print('-'*50)
+print('4 - Sair\n')
+
+option_menu = input('Digite a opção: ')
+print (f'\nOpção escolhida: {option_menu}')
