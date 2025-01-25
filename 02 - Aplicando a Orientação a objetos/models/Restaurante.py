0a8b375f35080c48e01f25e3ad458b8c6959d2e3
sthevanbello <sthevan.alves@gmail.com>
2025-01-25 17:24:54 -0300

    refact: Alteradas as propriedades para protected

--- 02 - Aplicando a Orientação a objetos/models/Restaurante.py
+++ 02 - Aplicando a Orientação a objetos/models/Restaurante.py
@@ -6,20 +6,34 @@ class Restaurante:
     - Nome
     - Categoria
     - ativo = False como padrão
+
+    Methods:
+    - listar_restaurantes()
     '''
     lista_restaurantes = []
     
     def __init__(self, nome, categoria):
         self.id = random.randint(1,1000)
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.capitalize()
+        self._ativo = False
         Restaurante.lista_restaurantes.append(self)
 
     def __str__(self):
-        return f'{str(self.id).ljust(5)}{self.nome.ljust(30)}{self.categoria.ljust(30)}{self.ativo}'
+        return f'{str(self.id).ljust(5)}{self._nome.ljust(30)}{self._categoria.ljust(30)}{self._ativo}'
 
     def listar_restaurantes():
+        '''
+        Método utilizado para listar todos os restaurantes inseridos
+        '''
+        print(f'{'Id'.ljust(5)}{'Nome'.ljust(30)}{'Categoria'.ljust(30)}Estado')
+        print('-'*100)
         for restaurante in Restaurante.lista_restaurantes:
             print(restaurante)
         print('='*100)
+
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Inativo'
+
+
--- 02 - Aplicando a Orientação a objetos/service.py
+++ 02 - Aplicando a Orientação a objetos/service.py
@@ -1,8 +1,8 @@
 from models.Restaurante import Restaurante
 
-restaurante_krusty = Restaurante('Krusty Burger', 'Fast food')
-restaurante_Moe = Restaurante('Bar do Moe', 'Boteco')
+restaurante_krusty = Restaurante('kruSty Burger', 'fast food')
+restaurante_Moe = Restaurante('bar do Moe', 'boteco')
 
 Restaurante.listar_restaurantes()
 
-print(restaurante_krusty)
+# print(restaurante_krusty)
