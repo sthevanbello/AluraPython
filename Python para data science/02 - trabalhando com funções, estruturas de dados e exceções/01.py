31cdcab9f78bfd40a84cfc2d8f361d4ba227b05d
sthevanbello <sthevan.alves@gmail.com>
2025-02-04 17:27:35 -0300

    list e Dic Comprehension

--- Python para data science/02 - trabalhando com funções, estruturas de dados e exceções/01.py
+++ Python para data science/02 - trabalhando com funções, estruturas de dados e exceções/01.py
@@ -45,10 +45,32 @@ def calcular_media(lista: list=[0]) -> float:
 
 # media = calcular_media(numeros)
 # print(media)
-qualitativo = 0.5
-numero_atualizado = lambda x: x + qualitativo
+# qualitativo = 0.5
+# numero_atualizado = lambda x: x + qualitativo
 
-print(numero_atualizado(6))
+# print(numero_atualizado(6))
 
-numeros_atualizados = map(lambda x: x + qualitativo, numeros)
-print(list(numeros_atualizados))
+# numeros_atualizados = map(lambda x: x + qualitativo, numeros)
+# print(list(numeros_atualizados))
+
+# alturas = [1.70, 1.80, 1.65, 1.75, 1.90]
+# pesos = [65, 80, 58, 70, 95]
+
+# imc = [round((peso / altura**2), 1) for altura, peso in zip(alturas, pesos)]
+# print(imc)
+
+nomes_estudantes = [ "Enrico Monteiro", "Luna Pereira", "Anthony Silveira", "Letícia Fernandes", 
+                    "João Vitor Nascimento", "Maysa Caldeira", "Diana Carvalho", "Mariane da Rosa",
+                    "Camila Fernandes", "Levi Alves", "Nicolas da Rocha", "Amanda Novaes", 
+                    "Laís Moraes", "Letícia Oliveira", "Lucca Novaes", "Lara Cunha", 
+                    "Beatriz Martins", "João Vitor Azevedo", "Stephany Rosa", "Gustavo Henrique Lima" ]
+
+medias_estudantes = [5.4, 4.1, 9.1, 5.3, 6.9, 3.1, 10.0, 5.0, 8.2, 5.5,
+                    8.1, 7.4, 5.0, 3.7, 8.1, 6.2, 6.1, 5.6, 6.7, 8.2]
+
+estudantes = {nomes_estudantes[i]: medias_estudantes[i] for i in range(len(nomes_estudantes)) if medias_estudantes[i] >= 9.0}
+print(estudantes)
+bolsistas = {nome: media 
+            for nome, media in zip(nomes_estudantes, medias_estudantes) 
+            if media >= 9.0}
+print(bolsistas)
